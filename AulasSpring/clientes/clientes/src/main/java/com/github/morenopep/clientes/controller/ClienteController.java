bf8d46dd17f81b136686cb2464bef82885f99780
Pedro <pedrowarsps4@gmail.com>
2021-03-06 17:43:57 -0300

    Add Metodo getById, e testado usando o Verbo GET

--- AulasSpring/clientes/clientes/src/main/java/com/github/morenopep/clientes/controller/ClienteController.java
+++ AulasSpring/clientes/clientes/src/main/java/com/github/morenopep/clientes/controller/ClienteController.java
@@ -2,11 +2,14 @@ package com.github.morenopep.clientes.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.github.morenopep.clientes.model.Cliente;
 import com.github.morenopep.clientes.repository.ClienteRepository;
@@ -23,4 +26,11 @@ public class ClienteController {
 		public Cliente salvar(@RequestBody Cliente cliente) {
 			return repository.save(cliente);
 		}
+
+	@GetMapping("{id}") //esse metodo vai buscar um cliente por id, se não encontrar vai mostrar um erro HttpStatus.NOT_FOUND
+		public Cliente getById(@PathVariable Long id) {
+			return repository.findById(id)
+					.orElseThrow( () -> new ResponseStatusException(HttpStatus.NOT_FOUND) );
+	}
 }
+
